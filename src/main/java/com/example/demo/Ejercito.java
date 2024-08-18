0239ac23b31a8cc93c620c67bd7a82fc15292322
Ivancho1253 <ivangonzalocabrera@gmail.com>
2024-08-18 17:49:56 -0300

    Se añadieron nuevos archivos java, y se eliminaron los default de IDX

---
+++ src/main/java/com/example/demo/Ejercito.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Ejercito {
+
+  private int vida;
+
+  public Ejercito(){
+  }
+
+  public Ejercito(int vida) {
+    this.vida = vida;
+  }
+
+  public int getVida() {
+    return vida;
+  }
+
+  public void setVida(int vida) {
+    this.vida = vida;
+  }
+
+}
---
+++ src/main/java/com/example/demo/Soldado.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Soldado 
+            extends Ejercito{
+    
+    public Soldado(int vida) {
+        super(vida);
+    }
+}
